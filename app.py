2ce211216507d2c46c37c3fe315675afb9748e1a
trace <ahmutysj3@gmail.com>
2023-05-17 12:50:53 -0400

    zip fuctions and lambdas and filters

--- app.py
+++ app.py
@@ -4,5 +4,19 @@ items = [
     ("Product3", 12),
 ]
 
-result = list(map(lambda item: item[1], items))
+result = list(filter(lambda item: item[1] <= 10, items))
 print(result)
+
+result2 = [item for item in items if item[1] <= 10]
+print(result2)
+
+list1 = [2 , 4 , 6]
+list2 = [10, 20, 30]
+print(list(zip("abc",range(3),list1,list2)))
+
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+browsing_session.pop()
+print("last number in browsing session stack", browsing_session[-1])
