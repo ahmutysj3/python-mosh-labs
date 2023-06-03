4a7ada81c71bf337d7dbf0abad0df23819c64ed7
Trace Ahmuty <ahmutysj3@gmail.com>
2023-06-03 13:19:15 -0400

    completed sets tutorial

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
-from array import array
+numbers = [1,1,2,4]
+numbers[2] = 3
 
-numbers = array("i",[1,2,3])
-numbers[0] = 1.0 # produces error
-print(numbers[0]) # produces error
+first = set(numbers)
+print(first)
+
+second = {1,5}
+print(second)
+
+union = first | second
+print(union)
+
+both = first & second
+print(both)
+
+difference = first - second
+print(difference)
+
+either = first ^ second
+print(either)
+
+if 1 in first:
+    print("its in there")
